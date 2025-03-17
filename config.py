e73c6d9908e3dc6a1702d1e35c3f38d0c4722f22
Yuri Ferreira <yurifronaldo.com>
2025-03-17 15:42:47 -0300

    Atualizando .gitignore para ignorar credenciais e ajustando config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.urandom(24)  # Necessário para segurança, pode ser qualquer valor
+    SECRET_KEY = os.getenv('SECRET_KEY')  # Necessário para segurança, pode ser qualquer valor
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'seu_email@gmail.com'  # Substitua com seu e-mail
-    MAIL_PASSWORD = 'sua_senha'  # Substitua com sua senha ou senha do app (se necessário)
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
