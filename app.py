5f68a9c054e077773ab3c28334435798df164207
Yuri Ferreira <yurifronaldo.com>
2025-03-17 15:17:51 -0300

    Tratamento de POST e GET adicionado na rota principal

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
-# Inicializa a aplicação Flask
 app = Flask(__name__)
 
-# Rota principal
-def index():
-    return render_template("index.html")
-
-# Verifica se o script está sendo executado diretamente
-if __name__ == "__main__":
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == "GET":
+        return render_template('index.html')
+    if request.method == "POST":
+        Email = {
+            'remetente': request.form['nome'],
+            'destinatario': request.form['email'],
+            'mensagem': request.form['mensagem'],
+        }
+        return Email
+if __name__ == '__main__':
     app.run(debug=True)
