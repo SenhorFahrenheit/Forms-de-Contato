85521ab57210fe86b290ec06ca446d632049ea92
Yuri Ferreira <yurifronaldo.com>
2025-03-17 21:59:17 -0300

    Credenciais Removidas

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print(os.getenv('MAIL_USERNAME'))
+print(os.getenv('MAIL_PASSWORD'))
+
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')  # Necessário para segurança, pode ser qualquer valor
+    SECRET_KEY = os.urandom(24)  # Necessário para segurança, pode ser qualquer valor
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
